c43784d446d75c049ee3e8a9242885406b908d50
Dima Toporets <Toporets.Dima.4002.10.90@gmail.com>
2020-07-05 16:00:31 +0300

    add styles and buttons

--- 001-tic-tac-toe/script.js
+++ 001-tic-tac-toe/script.js
@@ -13,5 +13,30 @@ function showScreen (screenName) {
   el.style.display = 'flex';
 }
 
-
 showScreen('menu');
+
+function darkTheme (screenName1,screenName2,screenName3,screenName4,screenName5) {
+  var el = document.getElementById(screenName1);
+  el.style.backgroundColor = "rgb(155, 55, 55)"
+  var el = document.getElementById(screenName2);
+  el.style.backgroundColor = "rgb(55, 155, 55)"
+  var el = document.getElementById(screenName3);
+  el.style.backgroundColor = "rgb(55, 55, 155)"
+  var el = document.getElementById(screenName4);
+  el.style.backgroundColor = "rgb(155, 155, 155)"
+  var el = document.getElementById(screenName5);
+  el.style.backgroundColor = "rgb(27, 155, 112)"
+}
+
+function lightTheme (screenName1,screenName2,screenName3,screenName4,screenName5) {
+  var el = document.getElementById(screenName1);
+  el.style.backgroundColor = "rgb(225, 155, 155)"
+  var el = document.getElementById(screenName2);
+  el.style.backgroundColor = "rgb(155, 255, 155)"
+  var el = document.getElementById(screenName3);
+  el.style.backgroundColor = "rgb(155, 155, 255)"
+  var el = document.getElementById(screenName4);
+  el.style.backgroundColor = "rgb(255, 255, 255)"
+  var el = document.getElementById(screenName5);
+  el.style.backgroundColor = "aquamarine"
+}
